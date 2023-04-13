200d62b6e9435df858bf3af1a94a99f04637dafa
Neo Jusha <neoprojectwork@gmail.com>
2023-04-13 09:06:46 +0200

    setting up react shopping cart app

---
+++ rdynamic-app/src/products.js
@@ -0,0 +1,53 @@
+import product1 from "./assets"
+
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Iphone",
+        price: 1000,
+        productImage: ""
+    },
+    {
+        id: 2,
+        productName: "Iphone",
+        price: 1000,
+        productImage: ""
+    },
+    {
+        id: 3,
+        productName: "Iphone",
+        price: 1000,
+        productImage: ""
+    },
+    {
+        id: 4,
+        productName: "Iphone",
+        price: 1000,
+        productImage: ""
+    },
+    {
+        id: 5,
+        productName: "Iphone",
+        price: 1000,
+        productImage: ""
+    },
+    {
+        id: 6,
+        productName: "Iphone",
+        price: 1000,
+        productImage: ""
+    },
+    {
+        id: 7,
+        productName: "Iphone",
+        price: 1000,
+        productImage: ""
+    },
+    {
+        id: 8,
+        productName: "Iphone",
+        price: 1000,
+        productImage: ""
+    }
+]
